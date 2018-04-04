f1d171fbd968a08b0b68fd0bb5096f91c01691b1
xunchao.cao <xunchao.cao@gmail.com>
2018-04-04 18:31:17 +0800

    add operation for docker-machine

--- docker/install.sh
+++ docker/install.sh
@@ -5,3 +5,8 @@ yum install -y yum-utils \
 yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 yum install docker-ce
+
+# install docker machine
+base=https://github.com/docker/machine/releases/download/v0.14.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo install /tmp/docker-machine /usr/local/bin/docker-machine
--- docker/rm.sh
+++ docker/rm.sh
@@ -10,11 +10,20 @@ yum remove docker \
            docker-engine \
            docker-ce
 
+# rm docker-machine
+rm $(which docker-machine)
 
 read -p "Del local images?(Y/N) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     rm -rf /var/lib/docker
-    echo done 
+fi
+
+read -p "Del all local docker machines?(Y/N) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    docker-machine rm -f $(docker-machine ls -q)
+    rm -rf /var/lib/docker
 fi
